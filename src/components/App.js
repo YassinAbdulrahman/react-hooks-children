9fbc74b2d2d56cefea8944a20aa6037769af52ff
YassinAbdulrahman <107113173+YassinAbdulrahman@users.noreply.github.com>
2023-02-27 00:06:51 +0300

    Practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Container from "./Container";
-
+import Example from "./Example";
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
+      <Header header="Hello, I'm in a container!"  description="I'm a description!" />
+      <Header header="I'm another container" description="Whoa that's weird!" />
+      <Header header="A third container!" description="Cray cray" />
       <Container header="Example header" direction="horizontal">
         <div>This is an example!</div>
         <div>This is an example!</div>
@@ -13,6 +17,10 @@ function App() {
         <div>This is an example!</div>
         <div>This is an example!</div>
       </Container>
+      <Example exampleProp="example value">
+        <h1>Example header!</h1>
+        <p>Some example text</p>
+      </Example>
       <Container header="Example header" textPosition="center">
         <img
           alt="turtle"
---
+++ src/components/Example.jsx
@@ -0,0 +1,5 @@
+function Example(props) {
+    return <div>{props.exampleProp}</div>;
+}
+
+export default Example
